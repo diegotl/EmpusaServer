41ca71b0d1ba76c721346978a7c797e1eb8c9603
Diego Trevisan Lara <diego@trevisa.nl>
2023-10-30 16:53:49 +0100

    Add public assets

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,5 +3,10 @@ import Vapor
 public func configure(_ app: Application) async throws {
     ContentConfiguration.global.use(encoder: JSONEncoder(), for: .json)
 
+    let file = FileMiddleware(
+        publicDirectory: app.directory.publicDirectory
+    )
+    app.middleware.use(file)
+
     try routes(app)
 }
