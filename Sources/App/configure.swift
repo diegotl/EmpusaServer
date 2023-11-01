cabe8aff1708f9c9b5e9c986e003d407e6c3f88f
Diego Trevisan Lara <diego@trevisa.nl>
2023-11-01 12:13:16 +0100

    Attempt to fix json decoding in linux.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,15 +1,6 @@
 import Vapor
 
 public func configure(_ app: Application) async throws {
-    let jsonEncoder = JSONEncoder()
-    jsonEncoder.dateEncodingStrategy = .iso8601
-
-    let jsonDecoder = JSONDecoder()
-    jsonDecoder.dateDecodingStrategy = .iso8601
-
-    ContentConfiguration.global.use(encoder: jsonEncoder, for: .json)
-    ContentConfiguration.global.use(decoder: jsonDecoder, for: .json)
-
     let file = FileMiddleware(
         publicDirectory: app.directory.publicDirectory
     )
